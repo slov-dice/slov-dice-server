87a0ce6f058a6b90d6b4a23fd0af4928dc0daabb
YaroslavWeb <yaroslavweb98@gmail.com>
2022-10-02 04:05:50 +0800

    add white list for cors

--- src/main.ts
+++ src/main.ts
@@ -5,9 +5,17 @@ import * as cookieParser from 'cookie-parser'
 import { AppModule } from './app.module'
 
 async function bootstrap() {
+  const whitelist = ['http://127.0.0.1:5173', 'http://localhost:5173']
+
   const app = await NestFactory.create(AppModule, {
     cors: {
-      origin: 'http://127.0.0.1:5173',
+      origin: function (origin, callback) {
+        if (!origin || whitelist.indexOf(origin) !== -1) {
+          callback(null, true)
+        } else {
+          callback(new Error('Not allowed by CORS'))
+        }
+      },
       credentials: true,
     },
   })
