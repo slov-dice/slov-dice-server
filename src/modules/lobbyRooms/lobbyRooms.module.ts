1008f85cc0d082d50866d55457db7d3ab31c7d7a
YaroslavWeb <yaroslavweb98@gmail.com>
2023-01-19 22:49:31 +0800

    update deps in lobbyRooms

--- src/modules/lobbyRooms/lobbyRooms.module.ts
+++ src/modules/lobbyRooms/lobbyRooms.module.ts
@@ -4,6 +4,13 @@ import { LobbyRoomsService } from './lobbyRooms.service'
 import { LobbyRoomsGateway } from './lobbyRooms.gateway'
 import { BattlefieldGateway } from './extensions/battlefield/battlefield.gateway'
 import { BattlefieldService } from './extensions/battlefield/battlefield.service'
+import { CharactersGateway } from './extensions/characters/characters.gateway'
+import { CharactersService } from './extensions/characters/characters.service'
+import { ChatGateway } from './extensions/chat/chat.gateway'
+import { ChatService } from './extensions/chat/chat.service'
+import { CommandsService } from './extensions/chat/commands.service'
+import { SettingsGateway } from './extensions/settings/settings.gateway'
+import { SettingsService } from './extensions/settings/settings.service'
 
 import { LobbyUsersModule } from 'modules/lobbyUsers/lobbyUsers.module'
 
@@ -14,6 +21,13 @@ import { LobbyUsersModule } from 'modules/lobbyUsers/lobbyUsers.module'
     LobbyRoomsService,
     BattlefieldGateway,
     BattlefieldService,
+    CharactersGateway,
+    CharactersService,
+    ChatGateway,
+    ChatService,
+    CommandsService,
+    SettingsGateway,
+    SettingsService,
   ],
   exports: [LobbyRoomsService],
 })
