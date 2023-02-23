12872a1f532e77f23faaef45173dd7b5aa89d86b
YaroslavWeb <yaroslavweb98@gmail.com>
2023-02-23 23:22:30 +0800

    update task maneger timer

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import * as cookieParser from 'cookie-parser'
 import { AppModule } from './app.module'
 
 async function bootstrap() {
-  const whitelist = ['http://127.0.0.1:5173', 'http://localhost:5173', 'https://slov-dice.com']
+  const whitelist = ['http://127.0.0.1:5173', 'http://127.0.0.1:4173', 'http://localhost:5173', 'https://slov-dice.com']
 
   const app = await NestFactory.create(AppModule, {
     cors: {
@@ -13,7 +13,6 @@ async function bootstrap() {
         if (!origin || whitelist.indexOf(origin) !== -1) {
           callback(null, true)
         } else {
-          console.log('(${origin})', origin)
           callback(new Error(`Not allowed by CORS`))
         }
       },
